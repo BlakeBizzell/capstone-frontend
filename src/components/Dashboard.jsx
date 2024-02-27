a18d04bfe91215e76a8a5aba43eb25387f536a10
bbizz <bbizz@DESKTOP-F1BIQ76>
2024-02-27 10:25:18 -0500

    front end up and going

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+}
+
+export default Dashboard;
